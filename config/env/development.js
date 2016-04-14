b8b66d1e6fc6129b138b9bba02aa52dbc94b85a7
Finger Nailbiter <iam.nailbiter@gmail.com>
2016-04-14 11:36:21 +0700

    Facebook Login API Enabled

--- config/env/development.js
+++ config/env/development.js
@@ -35,8 +35,8 @@ module.exports = {
     title: defaultEnvConfig.app.title + ' - Development Environment'
   },
   facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+    clientID: process.env.FACEBOOK_ID || '1689869297943320',
+    clientSecret: process.env.FACEBOOK_SECRET || 'dc078509fcccb8109bc55af6c6ee3e39',
     callbackURL: '/api/auth/facebook/callback'
   },
   twitter: {
