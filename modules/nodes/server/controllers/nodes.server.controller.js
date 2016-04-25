d9c94304f27efcedc122d2ecddd23a452ffb2d6a
Finger Nailbiter <iam.nailbiter@gmail.com>
2016-04-25 08:29:38 +0700

    Update Node Module
    Update node action #updated timestamp

--- modules/nodes/server/controllers/nodes.server.controller.js
+++ modules/nodes/server/controllers/nodes.server.controller.js
@@ -50,7 +50,7 @@ exports.update = function (req, res) {
   node.ip_address = req.body.ip_address;
   node.secret = req.body.secret;
   node.owner = req.body.owner ? req.body.owner : '';
-  node.updated = Date.now;
+  node.updated = new Date.now();
 
   node.save(function (err) {
     if (err) {
