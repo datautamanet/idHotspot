13d9fde00381fc1ca024120c04801b68106e89c5
Finger Nailbiter <iam.nailbiter@gmail.com>
2016-04-26 11:09:59 +0700

    Update start method

--- modules/core/client/services/facebook.client.service.js
+++ modules/core/client/services/facebook.client.service.js
@@ -4,18 +4,27 @@ angular.module('core').service('Facebook', ['Hotspot', '$rootScope',
   function (Hotspot, $rootScope) {
 
     this.inetStartShare = function (url) {
-      FB.ui({
-        method: 'share',
-        href: url
-      }, function (response) {
-        if (response && !response.error_message) {
-          // console.log($rootScope.nodes);
-          console.log('Content has been shared.');
-          Hotspot.start();
-        } else {
-          console.log('Error while posting.');
-        }
-      });
+      if ($rootScope.nodes) {
+        FB.ui({
+          method: 'share',
+          href: url
+        }, function (response) {
+          if (response && !response.error_message) {
+            // console.log($rootScope.nodes);
+            console.log('Content has been shared.');
+            Hotspot.start();
+          } else {
+            console.log('Error while posting.');
+          }
+        });
+      } else {
+        $.Notify({
+          caption: 'Internet Start Error',
+          content: 'Anda tidak berada di dalam jangkauan area idHotspot',
+          icon: '<b class="mif mif-location"></b>',
+          type: 'alert'
+        });
+      }
     };
 
   }
